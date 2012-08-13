8bf2ea3722b86c8274abe02efb75e3e518e70679
Daniel Werner <daniel.werner@wikimedia.de>
2012-08-13 02:53:25 +0200

    Added test for interaction with 'Loops' extension
    Change-Id: I3812aadf10cdca95e16f75d92e58a394d94d13a9

--- Arrays.php
+++ Arrays.php
@@ -37,7 +37,8 @@ $wgHooks['ParserFirstCallInit'][] = 'ExtArrays::init';
 $wgHooks['ParserClearState'   ][] = 'ExtArrays::onParserClearState';
 
 // parser tests registration:
-$wgParserTestFiles[] = ExtArrays::getDir() . "/arrayParserTests.txt";
+$wgParserTestFiles[] = ExtArrays::getDir() . '/arrayParserTests.txt';
+$wgParserTestFiles[] = ExtArrays::getDir() . '/arrayLoopsInteractionParserTests.txt';
 
 // Include the settings file:
 require_once ExtArrays::getDir() . '/Arrays.settings.php';
